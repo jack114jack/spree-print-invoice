e4f73c019cc2eaaae10aa609b8d846f9fea0247f
jack <jack114jack@gmail.com>
2019-12-03 11:16:24 +0530

    :store namespace removed from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Spree::Core::Engine.routes.append do
-	namespace :store do
-		namespace :admin do
-			get 'invoice/:id(/:template)', to: 'invoice#show'
-		end
+	namespace :admin do
+		get 'invoice/:id(/:template)', to: 'invoice#show'
 	end
 end
